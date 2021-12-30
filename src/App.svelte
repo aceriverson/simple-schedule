1832d38f42dbf0d2f4b583e7fd8858ae4afe96d2
Acer Iverson <aceriverson@college.harvard.edu>
2021-12-30 12:01:27 -0600

    Day layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,39 @@
 <script>
-	export let name;
+	import Day from './components/Day.svelte'
+
+	let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+	let widthScale = 24;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="days-container">
+		{#each days as day}
+		<div class="day-container">
+			<Day {day} />
+		</div>
+		{/each}
+	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		overflow-x: scroll;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.day-container {
+		padding: 0 .5em;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.days-container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	@media (min-width: 768px) {
+
 	}
 </style>
---
+++ src/components/Day.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let day;
+</script>
+
+<div class="main">
+    <h1>{day}</h1>
+</div>
+
+<style>
+	h1 {
+        text-align: center;
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 1.25em;
+        font-weight: 100;
+        padding: 1em;
+        border: 1px solid #ff3e00;
+    }
+
+    .main {
+        width: 18em;
+    }
+</style>
